1dac13caf8b708846262a8c37a1ab7856843a5e1
Alex Groleau <awgneo@xbetanet.com>
2013-07-24 14:54:25 -0400

    initial

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+Autoloader::add_classes(array(
+    'Icecast'   => __DIR__.'/classes/icecast.php',
+));
---
+++ classes/ichook.php
@@ -0,0 +1,67 @@
+<?php
+
+class Icecast {
+
+    private $host;
+    private $port;
+    private $username;
+    private $password;
+
+    private static $stats_path = '/admin/stats';
+
+    function __construct($host, $port, $username, $password) {
+
+        $this->host = $host;
+        $this->port = $port;
+        $this->username = $username;
+        $this->password = $password;
+
+    }
+
+    public function statistics() {
+
+        // create a new curl resource
+        $curl = curl_init();
+        // set url & port
+        curl_setopt($curl, CURLOPT_URL, $this->host . ':' . $this->port . self::$stats_path);
+        // set authentication parameters
+        curl_setopt($curl, CURLOPT_USERPWD, $this->username . ':' . $this->password);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        // return as a string
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        // output stats
+        $xml = curl_exec($curl);
+        // close curl resource to free up system resources
+        curl_close($curl);
+        // generate clean statistics from the output
+        $statistics = Format::forge($xml, 'xml')->to_array();
+        // success
+        return $statistics;
+
+    }
+
+    public function mount_statistics($mount) {
+
+        // get total server stats
+        $statistics = $this->statistics();
+        // get number of sources
+        $statistics_sources_count = $statistics['sources'];
+        // if we have no sources, we are done
+        if ($statistics_sources_count == '0')
+            return null;
+        // if we have one source, return it
+        if ($statistics_sources_count == '1')
+            return $statistics['source'];
+        // get all sources
+        $statistics_sources = $statistics['source'];
+        // we have many sources, find the right one
+        foreach ($statistics_sources as $statistics_source) {
+            if ($statistics_source['@attributes']['mount'] == $mount)
+                return $statistics_source;
+        }
+        // fail
+        return null;
+
+    }
+
+}
